9504f559f6d32f309bb569f5dca09c7ba06e78f1
john-shawkins <john.stephen.hawkins@gmail.com>
2023-01-06 22:05:36 +0100

    Added router and fixes some code

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -1,17 +1,26 @@
 import { Component, Output, EventEmitter } from '@angular/core';
 import {Task} from '../../Task';
+import {UiService} from "../../services/ui.service";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-add-task',
   templateUrl: './add-task.component.html',
   styleUrls: ['./add-task.component.css']
 })
+
 export class AddTaskComponent {
   @Output() onAddTask: EventEmitter<Task> = new EventEmitter<Task>();
 
   text!: string;
   day!: string;
   reminder: boolean = false;
+  showAddTask!: boolean;
+  subscription: Subscription;
+
+  constructor(private uiService: UiService) {
+    this.subscription = this.uiService.onToggle().subscribe(value => this.showAddTask = value)
+  }
 
   onSubmit() {
     if(!this.text) {
